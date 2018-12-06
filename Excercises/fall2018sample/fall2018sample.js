781fb44acca2e907f9cbc0ea88b5d20d3b4d0f70
Jeffrey Zimmer (RIT Student) <jrz4547@ad.rit.edu>
2018-12-06 16:30:29 -0500

    I have 100 percent now baby, I have such a big brain

--- Excercises/fall2018sample/fall2018sample.js
+++ Excercises/fall2018sample/fall2018sample.js
@@ -1,6 +1,18 @@
 /* Don't use <script> tags in a linked js file! */
 $(document).ready(function() {
     $(".menuitem").click(function(){
-        $(".submenu").toggle(1000);
+        $(this).next(".submenu").toggle(1000);
     });
 })
+
+let context = "content1.txt";
+let cString = `${context}`;
+        $("#choose-content").val(context);
+        $("#content").load(cString);
+
+        $("#choose-content").change(function() {
+            context = $(this).val();
+            cString = `${context}`;
+            $("#content").load(cString); 
+        });
+console.log(cString);
